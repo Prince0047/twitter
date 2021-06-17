72ff73786873058cb7383d509df168e951e6cf92
Prince0047 <sonubhadana838@gmail.com>
2021-06-17 19:25:22 +0530

    added feed and fixed bugs

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,5 +1,10 @@
 const { Sequelize } = require('sequelize');
 const UserModel = require('./models/User');
+const FollowerModel = require('./models/Follower');
+const TweetModel = require('./models/Tweet');
+const RetweetModel = require('./models/Retweet');
+const LikeModel = require('./models/Like');
+const CommentModel = require('./models/Comment');
 
 // Connect to database
 const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
@@ -18,7 +23,38 @@ const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
 })();
 
 const User = UserModel(sequelize);
+const Follower = FollowerModel(sequelize);
+const Tweet = TweetModel(sequelize);
+const Retweet = RetweetModel(sequelize);
+const Like = LikeModel(sequelize);
+const Comment = CommentModel(sequelize);
+
+// User -> Follower association
+User.hasMany(Follower, { as: 'Followers', foreignKey: 'follower' });
+User.hasMany(Follower, { as: 'Following', foreignKey: 'followed' });
+
+// User -> Tweet association
+User.hasMany(Tweet, { foreignKey: 'userId' });
+// User -> Like association
+User.hasMany(Like, { foreignKey: 'userId' });
+// User -> Retweet association
+User.hasMany(Retweet, { foreignKey: 'userId' });
+// Tweet -> Like association
+Tweet.hasMany(Like, { foreignKey: 'tweetId' });
+// Tweet -> Retweet association
+Tweet.hasMany(Retweet, { foreignKey: 'tweetId' });
+// User -> Comment association
+User.hasMany(Comment, { foreignKey: 'userId' });
+// Tweet -> Bookmark association
+// Tweet.hasMany(Bookmark, { foreignKey: 'tweetId' });
+// User.hasMany(Bookmark, { foreignKey: 'userId' });
 
 module.exports = {
   User,
+  Follower,
+  Tweet,
+  Retweet,
+  Like,
+  Comment,
+  sequelize,
 };
