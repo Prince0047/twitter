a6def8418db32ef3cc91764baec6bb9a597eefac
Prince0047 <sonubhadana838@gmail.com>
2021-06-30 18:50:26 +0530

    added tweet/comment validation

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -13,4 +13,30 @@ module.exports = {
     const { error, value } = schema.validate(data);
     return { error, value };
   },
+  addTweetValidation: (data) => {
+    const schema = Joi.object({
+      userId: Joi.string().required(),
+      text: Joi.string().required(),
+      media: Joi.object().optional(),
+      resource_type: Joi.string().optional(),
+    }).options({ abortEarly: false });
+    const { error, value } = schema.validate(data);
+    return { error, value };
+  },
+  addRetweetValidation: (data) => {
+    const schema = Joi.object({
+      userId: Joi.string().required(),
+      tweetId: Joi.string().required(),
+    }).options({ abortEarly: false });
+    const { error, value } = schema.validate(data);
+    return { error, value };
+  },
+  bookmarkValidation: (data) => {
+    const schema = Joi.object({
+      userId: Joi.string().required(),
+      tweetId: Joi.string().required(),
+    }).options({ abortEarly: false });
+    const { error, value } = schema.validate(data);
+    return { error, value };
+  },
 };
